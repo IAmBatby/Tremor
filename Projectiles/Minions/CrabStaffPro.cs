779f45c3ffd731cecafa7d8b225be9321b597eb1
JavidPack <javidpack@gmail.com>
2017-08-02 17:45:24 -0600

    Fixes summon BabySlime+Crab then desummon BabySlime buff problem. (#14)
    * Remove bad setdefaults, noted a bug, fixed Unload to restore textures.
    
    * Fixes summon BabySlime+Crab then desummon BabySlime buff problem.

--- Projectiles/Minions/CrabStaffPro.cs
+++ Projectiles/Minions/CrabStaffPro.cs
@@ -8,41 +8,39 @@ namespace Tremor.Projectiles.Minions
 	{
 		public override void SetDefaults()
 		{
+			projectile.width = 32;
+			projectile.height = 24;
+			projectile.netImportant = true;
+			projectile.friendly = true;
+			projectile.minionSlots = 1;
+			projectile.aiStyle = 26;
+			projectile.timeLeft = 18000;
+			Main.projFrames[projectile.type] = 1;
+			ProjectileID.Sets.MinionSacrificable[projectile.type] = true;
+			projectile.penetrate = -1;
+			projectile.timeLeft *= 5;
+			projectile.minion = true;
+			aiType = 266;
+			projectile.tileCollide = false;
+			ProjectileID.Sets.MinionTargettingFeature[projectile.type] = true;
+		}
 
-            projectile.width = 32;
-            projectile.height = 24;
-            projectile.netImportant = true;
-            projectile.friendly = true;
-            projectile.minionSlots = 1;
-            projectile.aiStyle = 26;
-            projectile.timeLeft = 18000;
-            Main.projFrames[projectile.type] = 1;
-            ProjectileID.Sets.MinionSacrificable[projectile.type] = true;
-            projectile.penetrate = -1;
-            projectile.timeLeft *= 5;
-            projectile.minion = true;
-            aiType = 266;
-            projectile.tileCollide = false;
-            ProjectileID.Sets.MinionTargettingFeature[projectile.type] = true;
-        }
-
-    public override void SetStaticDefaults()
-    {
-      DisplayName.SetDefault("Crab Staff");
-       
-    }
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Crab Staff");
+		}
 
 
-public override bool TileCollideStyle(ref int width, ref int height, ref bool fallThrough) 
-{ 
-fallThrough = false; 
-return true; 
-}
+		public override bool TileCollideStyle(ref int width, ref int height, ref bool fallThrough)
+		{
+			fallThrough = false;
+			return true;
+		}
 
-        public override void CheckActive()
+		public override void CheckActive()
 		{
 			Player player = Main.player[projectile.owner];
-            TremorPlayer modPlayer = (TremorPlayer)player.GetModPlayer(mod, "TremorPlayer");
+			TremorPlayer modPlayer = (TremorPlayer)player.GetModPlayer(mod, "TremorPlayer");
 			if (player.dead)
 			{
 				modPlayer.crabStaff = false;
@@ -53,14 +51,26 @@ return true;
 			}
 		}
 
+		public override bool OnTileCollide(Vector2 oldVelocity)
+		{
+			if (projectile.penetrate == 0)
+			{
+				projectile.Kill();
+			}
+			return false;
+		}
 
-        public override bool OnTileCollide(Vector2 oldVelocity)
-        {
-            if (projectile.penetrate == 0)
-            {
-                projectile.Kill();
-            }
-            return false;
-        }
-    }
+		private bool oldSlime;
+		public override bool PreAI()
+		{
+			oldSlime = Main.player[projectile.owner].slime;
+			Main.player[projectile.owner].slime = false;
+			return base.PreAI();
+		}
+
+		public override void PostAI()
+		{
+			Main.player[projectile.owner].slime = oldSlime;
+		}
+	}
 }
