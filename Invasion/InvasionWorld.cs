d43cc101c21bfe234721adf0fe306e009855a3f2
Jofairden <danielzondervan.nl@gmail.com>
2017-08-03 15:21:01 +0200

    InvasionWorld fix, potential bug fix

--- Invasion/InvasionWorld.cs
+++ Invasion/InvasionWorld.cs
@@ -9,29 +9,14 @@ namespace Tremor.Invasion
 {
 	public class InvasionWorld : ModWorld
 	{
-		private const int saveVersion = 0;
-		public static int CyberWrathPoints = 0;
+		public static int CyberWrathPoints = 0; // Is set once to Points1, never used. Delete ?? (?)
 		public static int CyberWrathPoints1;
 		public static bool CyberWrath;
-		public override void ModifyWorldGenTasks(List<GenPass> tasks, ref float totalWeight)
-		{
-
-		}
-
-		public override void PostWorldGen()
-		{
-
-		}
 
 		public override void Initialize()
 		{
 			CyberWrathPoints1 = 0;
 			CyberWrath = false;
-		}
-
-		public override void TileCountsAvailable(int[] tileCounts)
-		{
-
 		}
 
 		/*public override void SaveCustomData(BinaryWriter writer)
@@ -43,11 +28,20 @@ namespace Tremor.Invasion
 
 		public override TagCompound Save()
 		{
-			TagCompound save_data = new TagCompound();
-			save_data.Add("CyberWrath", CyberWrath);
-			save_data.Add("CyberWrathPoints1", CyberWrathPoints1);
-			return save_data;
+			var tc = new TagCompound
+			{
+				{"CyberWrath", CyberWrath},
+				{"CyberWrathPoints1", CyberWrathPoints1}
+			};
+			return tc;
 		}
+
+		public override void Load(TagCompound tag)
+		{
+			CyberWrath = tag.GetBool("CyberWrath");
+			CyberWrathPoints1 = tag.GetAsInt("CyberWrathPoints1");
+		}
+
 
 		public override void NetSend(BinaryWriter writer)
 		{
@@ -57,23 +51,17 @@ namespace Tremor.Invasion
 
 		public override void NetReceive(BinaryReader reader)
 		{
-			BitsByte flags = reader.ReadByte();
-			flags[0] = CyberWrath;
+			CyberWrath = reader.ReadBoolean();
 			CyberWrathPoints1 = reader.ReadInt32();
 		}
 
-		int num;
-		public override void Load(TagCompound tag)
-		{
-			CyberWrath = tag.GetBool("CyberWrath");
-			CyberWrathPoints1 = tag.GetInt("CyberWrathPoints1");
-		}
 
 		public override void LoadLegacy(BinaryReader reader)
 		{
 			int loadVersion = reader.ReadInt32();
 			if (loadVersion == 0)
 			{
+				// ? potential trouble
 				BitsByte flags = reader.ReadByte();
 				CyberWrath = flags[0];
 				CyberWrathPoints1 = reader.ReadInt32();
