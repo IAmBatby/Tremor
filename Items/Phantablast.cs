92c62bb5a0fbe4d231c1a99d4584821048fa61e0
Jofairden <danielzondervan.nl@gmail.com>
2017-08-27 14:39:50 +0200

    Improved Phantablast

--- Items/Phantablast.cs
+++ Items/Phantablast.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Runtime.InteropServices;
 using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
@@ -5,12 +7,10 @@ using Terraria.ModLoader;
 
 namespace Tremor.Items
 {
-
 	public class Phantablast : ModItem
 	{
 		public override void SetDefaults()
 		{
-
 			item.damage = 83;
 			item.ranged = true;
 			item.width = 20;
@@ -20,11 +20,11 @@ namespace Tremor.Items
 			item.useStyle = 5;
 			item.knockBack = 6;
 			item.value = 210000;
-			item.rare = 10;
+			item.rare = AmmoID.Bullet;
 			item.useStyle = 5;
 			item.UseSound = SoundID.Item36;
 			item.noMelee = true;
-			item.autoReuse = false;
+			item.autoReuse = true;
 			item.shoot = 10;
 			item.shootSpeed = 23f;
 			item.useAmmo = AmmoID.Bullet;
@@ -33,7 +33,7 @@ namespace Tremor.Items
 		public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Phantablast");
-			Tooltip.SetDefault("");
+			Tooltip.SetDefault("Fires your ammo in a blast\n50% chance to not consume ammo");
 		}
 
 
@@ -44,21 +44,24 @@ namespace Tremor.Items
 
 		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
 		{
-			for (int i = 0; i < 1; ++i) // Will shoot 3 bullets.
+			for (int i = 1; i < 6; i++)
 			{
-				Projectile.NewProjectile(position.X, position.Y, speedX + 2, speedY + 3, type, damage, knockBack, Main.myPlayer);
-				Projectile.NewProjectile(position.X, position.Y, speedX, speedY, type, damage, knockBack, Main.myPlayer);
-				Projectile.NewProjectile(position.X, position.Y, speedX - 2, speedY - 3, type, damage, knockBack, Main.myPlayer);
-				Projectile.NewProjectile(position.X, position.Y, speedX - 4, speedY - 2, type, damage, knockBack, Main.myPlayer);
-				Projectile.NewProjectile(position.X, position.Y, speedX + 4, speedY + 2, type, damage, knockBack, Main.myPlayer);
+				// Randomize speed
+				Vector2 velocity = new Vector2(speedX, speedY) * Main.rand.NextFloat(0.90f - i/100f, 1.10f + i/100f);
+				// Randomize the angle
+				float spread = MathHelper.ToRadians(5f);
+				float baseSpeed = (float)Math.Sqrt(velocity.X * velocity.X + velocity.Y * velocity.Y);
+				double baseAngle = velocity.ToRotation();
+				double randomAngle = baseAngle + (Main.rand.NextFloat() - 0.5f) * spread;
+				velocity = ((float)randomAngle).ToRotationVector2() * baseSpeed;
+				// Shoot
+				Projectile.NewProjectile(position.X, position.Y, velocity.X, velocity.Y, type, damage, knockBack, Main.myPlayer);
 			}
 			return false;
 		}
 
 		public override bool ConsumeAmmo(Player p)
-		{
-			return Main.rand.Next(2) == 0;
-		}
+			=> Main.rand.NextBool();
 
 		public override void AddRecipes()
 		{
