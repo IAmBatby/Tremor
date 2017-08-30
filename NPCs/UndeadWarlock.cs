b5b162efb1c94a995d6910ed3526132d491902aa
Jofairden <danielzondervan.nl@gmail.com>
2017-08-30 15:18:45 +0200

    Re-added the UndeadWarlock, which was missing

---
+++ NPCs/UndeadWarlock.cs
@@ -0,0 +1,82 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+using Microsoft.Xna.Framework;
+
+namespace Tremor.NPCs
+{
+	public class UndeadWarlock : ModNPC
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Undead Warlock");
+			Main.npcFrameCount[npc.type] = 15;
+		}
+
+		const int ShootRate = 250;
+		const int ShootDamage = 20;
+		const float ShootKN = 1.0f;
+		const float ShootSpeed = 4;
+
+		int TimeToShoot = 0;
+
+		public override void SetDefaults()
+		{
+			npc.width = 28;
+			npc.height = 46;
+			npc.damage = 20;
+			npc.defense = 11;
+			npc.lifeMax = 340;
+			npc.HitSound = SoundID.NPCHit1;
+			npc.DeathSound = SoundID.NPCDeath2;
+			npc.value = Item.buyPrice(0, 3, 6, 2);
+			npc.knockBackResist = 1f;
+			npc.aiStyle = 3;
+			aiType = 524;
+			animationType = 21;
+
+			TimeToShoot = 0;
+		}
+
+		public override void AI()
+		{
+			if (Main.netMode != 1 && TimeToShoot++ >= ShootRate && npc.target != -1)
+			{
+				Vector2 velocity = Vector2.Normalize(Main.player[npc.target].Center - npc.Center) * ShootSpeed;
+				Projectile.NewProjectile(npc.Center.X, npc.Center.Y, velocity.X, velocity.Y, ProjectileID.CursedFlameHostile, ShootDamage, ShootKN);
+
+				TimeToShoot = 0;
+			}
+
+			if (Main.netMode != 1 && Main.rand.Next(210) == 0)
+				NPC.NewNPC((int)npc.position.X + 50, (int)npc.position.Y, NPCID.CursedSkull);
+
+			for (int i = npc.oldPos.Length - 1; i > 0; i--)
+				npc.oldPos[i] = npc.oldPos[i - 1];
+			npc.oldPos[0] = npc.position;
+		}
+
+		public override void HitEffect(int hitDirection, double damage)
+		{
+			if (npc.life <= 0)
+			{
+				for (int k = 0; k < 20; k++)
+					Dust.NewDust(npc.position, npc.width, npc.height, 151, 2.5f * hitDirection, -2.5f, 0, default(Color), 0.7f);
+
+				Dust.NewDust(npc.position, npc.width, npc.height, 151, 2.5f * hitDirection, -2.5f, 0, default(Color), 0.7f);
+				Dust.NewDust(npc.position, npc.width, npc.height, 151, 2.5f * hitDirection, -2.5f, 0, default(Color), 0.7f);
+				Dust.NewDust(npc.position, npc.width, npc.height, 151, 2.5f * hitDirection, -2.5f, 0, default(Color), 0.7f);
+				Gore.NewGore(npc.position, npc.velocity, mod.GetGoreSlot("Gores/UWGore1"), 1f);
+				Gore.NewGore(npc.position, npc.velocity, mod.GetGoreSlot("Gores/UWGore2"), 1f);
+				Gore.NewGore(npc.position, npc.velocity, mod.GetGoreSlot("Gores/UWGore2"), 1f);
+				Gore.NewGore(npc.position, npc.velocity, mod.GetGoreSlot("Gores/UWGore3"), 1f);
+				Gore.NewGore(npc.position, npc.velocity, mod.GetGoreSlot("Gores/UWGore3"), 1f);
+			}
+		}
+
+
+		public override float SpawnChance(NPCSpawnInfo spawnInfo)
+			=> Helper.NormalSpawn(spawnInfo) && Helper.NoZoneAllowWater(spawnInfo) && NPC.downedBoss3 && !Main.dayTime && spawnInfo.spawnTileY < Main.worldSurface ? 0.008f : 0f;
+	}
+}
