3188d862ccfe2bc690d6b4408c315f2ceec3eba9
Jofairden <danielzondervan.nl@gmail.com>
2017-10-15 15:56:01 +0200

    TremorAbstractItem

---
+++ TremorAbstractItem.cs
@@ -0,0 +1,33 @@
+﻿using Terraria.ModLoader;
+
+namespace Tremor
+{
+	public abstract class TremorAbstractItem : ModItem
+	{
+		protected abstract void Defaults();
+
+		protected abstract void StaticDefaults();
+
+		public virtual void SafeDefaults()
+		{
+
+		}
+
+		public virtual void SafeStaticDefaults()
+		{
+
+		}
+
+		public sealed override void SetDefaults()
+		{
+			Defaults();
+			SafeDefaults();
+		}
+
+		public sealed override void SetStaticDefaults()
+		{
+			StaticDefaults();
+			SafeStaticDefaults();
+		}
+	}
+}
